f34221f56abe3606b6327f9869897d9f2c5381bc
Cherch <b.cherchiglia@gmail.com>
2023-08-04 16:16:38 -0400

    bio time

--- meds/src/App.jsx
+++ meds/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useRef, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Tour from './pages/Tour'
+import Bio from './pages/Bio'
 import Nav from './components/Nav'
 import './App.css'
 
@@ -22,6 +23,7 @@ function App() {
         <Routes >
           <Route path="/" element={<Home />} />
           <Route path="/tour" element={<Tour />} />
+          <Route path="/bio" element={<Bio />} />
         </Routes>
       </main>
     </div>
---
+++ meds/src/pages/Bio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bio = () => {
+  return (
+    <div>
+      <h3>about the band</h3>
+    </div>
+  )
+}
+
+export default Bio
