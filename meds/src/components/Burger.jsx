c4d55669ee6e87efdf933a9a7d7fd97bef652c65
Cherch <b.cherchiglia@gmail.com>
2023-08-12 18:51:24 -0400

    burgertime

---
+++ meds/src/components/Burger.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Burger = () => {
+  return (
+    <div className="burger">
+        <div className="pickle pickle1"/>
+        <div className="pickle pickle2"/>
+        <div className="pickle pickle3"/>
+      
+    </div>
+  )
+}
+
+export default Burger
