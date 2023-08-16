6a15c6f6625eb8f2557ed623a1a5247904e58903
Cherch <b.cherchiglia@gmail.com>
2023-08-16 10:49:12 -0400

    bio css edits

--- meds/src/pages/Bio.jsx
+++ meds/src/pages/Bio.jsx
@@ -9,38 +9,39 @@ const Bio = () => {
             <h3 className="headerText">Official Bio</h3>
             <img className="medsImage" src="/pesoscherchBB.webp" alt="cherchBB" />
             <br />
-            <p>
-                <strong className="boldBioText">Meds</strong>
-                began as an experiment.
-            </p>
-            <p>
-                In an effort to compose in more avant garde theatres, Brooklyn based multi-instrumentalist<strong className="boldBioText">Cherch</strong>(given name: Brian Cherchiglia) went in.
-            </p>
-            <p>
-                <strong className="boldBioText">
-                    Cherch
-                </strong>
-                sought to combine his love of hip-hop production elements with his notorious guitar work; the <i> New York Times & Village Voice </i> heralded composer enlisted several of his best collaborators (featuring members of <i>The Bottom Dollars, God Tiny, Hollis Brown, Catty</i> and more) and entered the studio with world-renowned producer Jeff Berner (<i>Psychic TV, Shilpa Ray</i>), resulting in the debut recording
-                <strong className="boldText">
+            <div className="bioTextCard">
+                <p>
+                    <strong className="boldBioText">Meds</strong>
+                    began as an experiment.
+                </p>
+                <p>
+                    In an effort to compose in more avant garde theatres, Brooklyn based multi-instrumentalist<strong className="boldBioText">Cherch</strong>(given name: Brian Cherchiglia) went in.
+                </p>
+                <p>
+                    <strong className="boldBioText">
+                        Cherch
+                    </strong>
+                    sought to combine his love of hip-hop production elements with his notorious guitar work; the <i> New York Times & Village Voice </i> heralded composer enlisted several of his best collaborators (featuring members of <i>The Bottom Dollars, God Tiny, Hollis Brown, Catty</i> and more) and entered the studio with world-renowned producer Jeff Berner (<i>Psychic TV, Shilpa Ray</i>), resulting in the debut recording
+                    <strong className="boldText">
+                        <i>
+                            Night (Volume 1).
+                        </i>
+                    </strong>
+                </p>
+                <p>
+                    <strong >
+                        <i>
+                            Night (Volume 1)
+                        </i>
+                    </strong>
+                    : is a short vignette of weaving, ornate compositions themed around trauma survival and mental health advocacy.<strong className="boldBioText">Meds</strong>live somewhere between the psychedelia of <i>Spiritualized and TV On the Radio </i> and the indie punk joie de vivre of
                     <i>
-                        Night (Volume 1).
+                        The Replacements, The Cure and The War on Drugs.
                     </i>
-                </strong>
-            </p>
-            <p>
-                <strong >
-                    <i>
-                        Night (Volume 1)
-                    </i>
-                </strong>
-                : is a short vignette of weaving, ornate compositions themed around trauma survival and mental health advocacy.<strong className="boldBioText">Meds</strong>live somewhere between the psychedelia of <i>Spiritualized and TV On the Radio </i> and the indie punk joie de vivre of
-                <i>
-                    The Replacements, The Cure and The War on Drugs.
-                </i>
-            </p>
-            <p>
-                The first single, “An Unexpected Visit to the Isabella Stewart Gardner Museum,” will be available on all digital streaming platforms worldwide in late 2023.
-            </p>
+                </p>
+                <p>
+                    The first single, “An Unexpected Visit to the Isabella Stewart Gardner Museum,” will be available on all digital streaming platforms worldwide in late 2023.
+                </p>
             <br />
             <p>
                 <strong className="medsText">
@@ -88,6 +89,7 @@ const Bio = () => {
                     <Link to="https://dreamcymbals.com/artists/usa/berg-evan-buffalo" target="_blank" className="contactA" >Dream Cymbals</Link>
                     <br />
                 </p>
+            </div>
             </div>
             <br />
             <br />
