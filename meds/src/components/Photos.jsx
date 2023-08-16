9ec750b92262e7780dba6a49f1bd8ef1694489b4
Cherch <b.cherchiglia@gmail.com>
2023-08-16 09:30:08 -0400

    minor css adjustments

--- meds/src/components/Photos.jsx
+++ meds/src/components/Photos.jsx
@@ -13,55 +13,55 @@ const Photos = () => {
             <figure>
                 <Link target="_blank" to="http://medsofficial.bandcamp.com">
                     <img className="medsImage" src="/pesoscherchBB.webp" alt="cherchBB" />
-                    <figcaption>© {getYear()} Nicholas Fallon, all rights reserved.</figcaption>
+                    <figcaption className="captionA">© {getYear()} Nicholas Fallon, all rights reserved.</figcaption>
                 </Link>
             </figure>
             <figure>
                 <Link target="_blank" to="http://medsofficial.bandcamp.com">
                     <img className="medsImage" src="/tbd_dd1.jpg" alt="bb1Image" />
-                    <figcaption>© {getYear()} Dana Distortion, all rights reserved.</figcaption>
+                    <figcaption className="captionA">© {getYear()} Dana Distortion, all rights reserved.</figcaption>
                 </Link>
             </figure>
             <figure>
                 <Link target="_blank" to="http://medsofficial.bandcamp.com">
                     <img className="medsImage" src="/The-Bottom-Dollars-29-868x580.jpg" alt="bb1Image" />
-                    <figcaption>© {getYear()} Nicholas Fallon, all rights reserved.</figcaption>
+                    <figcaption className="captionA">© {getYear()} Nicholas Fallon, all rights reserved.</figcaption>
                 </Link>
             </figure>
             <figure>
                 <Link target="_blank" to="http://medsofficial.bandcamp.com">
                     <img className="medsImage" src="/the bottom dollars-45.jpg" alt="bb1Image" />
-                    <figcaption>© {getYear()} Kylie Squiers, all rights reserved.</figcaption>
+                    <figcaption className="captionA">© {getYear()} Kylie Squiers, all rights reserved.</figcaption>
                 </Link>
             </figure>
             <figure>
                 <Link target="_blank" to="http://medsofficial.bandcamp.com">
                     <img className="medsImage" src="https://ef4537.p3cdn1.secureserver.net/wp-content/uploads/2015/01/The-Bottom-Dollars-40-868x580.jpg" alt="bb2Image" />
-                    <figcaption>© {getYear()} Nicholas Fallon, all rights reserved.</figcaption>
+                    <figcaption className="captionA">© {getYear()} Nicholas Fallon, all rights reserved.</figcaption>
                 </Link>
             </figure>
             <figure>
                 <Link target="_blank" to="http://medsofficial.bandcamp.com">
                     <img className="medsImage" src="/the bottom dollars-50.jpg" alt="bergGuitImage" />
-                    <figcaption>© {getYear()} Kylie Squiers, all rights reserved.</figcaption>
+                    <figcaption className="captionA">© {getYear()} Kylie Squiers, all rights reserved.</figcaption>
                 </Link>
             </figure>
             <figure>
                 <Link target="_blank" to="http://medsofficial.bandcamp.com">
                     <img className="medsImage" src="/the bottom dollars-48.jpg" alt="jerImage" />
-                    <figcaption>© {getYear()} Kylie Squiers, all rights reserved.</figcaption>
+                    <figcaption className="captionA">© {getYear()} Kylie Squiers, all rights reserved.</figcaption>
                 </Link>
             </figure>
             <figure>
                 <Link target="_blank" to="http://medsofficial.bandcamp.com">
                     <img className="medsImage" src="/the bottom dollars-49.jpg" alt="chrisImage" />
-                    <figcaption>© {getYear()} Kylie Squiers, all rights reserved.</figcaption>
+                    <figcaption className="captionA">© {getYear()} Kylie Squiers, all rights reserved.</figcaption>
                 </Link>
             </figure>
             <figure>
                 <Link target="_blank" to="http://medsofficial.bandcamp.com">
                     <img className="medsImage" src="/the bottom dollars-55.jpg" alt="cherchImage" />
-                    <figcaption>© {getYear()} Kylie Squiers, all rights reserved.</figcaption>
+                    <figcaption className="captionA">© {getYear()} Kylie Squiers, all rights reserved.</figcaption>
                 </Link>
             </figure>
         </div>
