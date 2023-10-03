125ba13040ba4b4ddab53db0a4b24d8bf1f762d9
Cherch <b.cherchiglia@gmail.com>
2023-10-03 17:56:42 -0400

    tour date added

--- meds/src/pages/Tour.jsx
+++ meds/src/pages/Tour.jsx
@@ -20,6 +20,21 @@ const Tour = () => {
                         </thead>
                         <tbody>
                             <tr>
+                                <td>10.15.2023</td>
+                                <td>Sleepwalk</td>
+                                <td>Brooklyn, NY</td>
+                                <td className="td">
+                                    <Link target="_blank" to="https://sleepwalk.nyc/">
+                                        <button className="onSaleButton">on sale</button>
+                                    </Link>
+                                </td>
+                                <td>
+                                    <Link target="_blank" to="https://sleepwalk.nyc/">
+                                        <button className="onSaleButton">details</button>
+                                    </Link>
+                                </td>
+                            </tr>
+                            <tr>
                                 <td>10.27.2023</td>
                                 <td>The Gutter</td>
                                 <td>Brooklyn, NY</td>
