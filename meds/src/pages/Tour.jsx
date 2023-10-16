1453b995963de4474801d9afaebbb46b6888c82a
Cherch <b.cherchiglia@gmail.com>
2023-10-16 07:30:03 -0400

    tour date removed

--- meds/src/pages/Tour.jsx
+++ meds/src/pages/Tour.jsx
@@ -20,25 +20,9 @@ const Tour = () => {
                         </thead>
                         <tbody>
                             <tr>
-                                <td>10.15.2023</td>
-                                <td>Sleepwalk
-                                    [solo set]
-                                </td>
-                                <td>Brooklyn, NY</td>
-                                <td className="td">
-                                    <Link target="_blank" to="https://link.dice.fm/bc83f6d94b43">
-                                        <button className="onSaleButton">on sale</button>
-                                    </Link>
-                                </td>
-                                <td>
-                                    <Link target="_blank" to="https://sleepwalk.nyc/">
-                                        <button className="onSaleButton">details</button>
-                                    </Link>
-                                </td>
-                            </tr>
-                            <tr>
                                 <td>10.27.2023</td>
                                 <td> The Gutter
+                                    <br />
                                     [🎃 set as Gorillaz] 
                                 </td>
                                 <td>Brooklyn, NY</td>
