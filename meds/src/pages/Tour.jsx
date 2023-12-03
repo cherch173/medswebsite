58b3ca5a68e6b8839f812d00b46bb74a76a7096f
Cherch <b.cherchiglia@gmail.com>
2023-12-03 16:12:13 -0500

    Links added instead of p tags in tour dates

--- meds/src/pages/Tour.jsx
+++ meds/src/pages/Tour.jsx
@@ -24,18 +24,30 @@ const Tour = () => {
                                     FRI JAN 05, 2024
                                 </td>
                                 <td>
-                                    <p className="venueTD">Alphaville</p>
+                                    <p className="venueTD">
+                                        <Link target="_blank" className="supportA" to="https://www.instagram.com/alphavillebk">
+                                            Alphaville
+                                        </Link>
+                                    </p>
                                 </td>
                                 <td className="td">
                                     Brooklyn, NY
                                 </td>
                                 <td className="td">
-                                    <i>Cold Sugar
+                                    <i>
+                                        <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/2AUvhMfDlJMpAyPIHy9i8a?si=cc-L5MYNSouzb22zZg5D6w">
+                                            Cold Sugar
+                                        </Link>
                                         <br />
-                                        Brunch (Burlington, VT)
+                                        <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/20u1eNEFrgn9XCwUGuBNt9?si=HysKacO6QIeVijRbyMB4Qg">
+                                            Brunch (Burlington, VT)
+                                        </Link>
                                         <br />
-                                         The Alphaville All-Stars</i>
-                                        <br />
+                                        <Link className="supportA" target="_blank" to="http://www.instagram.com/alphavillebk">
+                                            The Alphaville All-Stars
+                                        </Link>
+                                    </i>
+                                    <br />
                                 </td>
                                 <td className="td">
                                     <Link target="_blank" to="https://www.alphavillebk.com/">
@@ -47,17 +59,25 @@ const Tour = () => {
                             <tr>
                                 <td className="td">
                                     TUE JAN 30, 2024
-                                    </td>
+                                </td>
                                 <td>
-                                    <p className="venueTD">Sleepwalk</p>
+                                    <p className="venueTD">
+                                        <Link target="_blank" className="supportA" to="https://www.instagram.com/sleepwalk.nyc">
+                                            Sleepwalk
+                                        </Link>
+                                    </p>
                                 </td>
                                 <td className="td">
                                     Brooklyn, NY
                                 </td>
                                 <td className="td">
-                                    <i>&nbsp;Nite Music 
+                                    <i>
+                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/3kQ55Rw5xE19eiAV3hLROi?si=KhsxCIZmSTGMp4nfZw6kZg">
+                                            Nite Music
+                                        </Link>
                                         <br />
-                                        + TBA&nbsp;</i>
+                                        & TBA
+                                    </i>
 
                                 </td>
                                 <td className="td">
