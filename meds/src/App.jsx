b320d3a3aed019181515d754841140217d282f98
Cherch <b.cherchiglia@gmail.com>
2023-12-04 07:29:19 -0500

    google analytics JSX setup at root

--- meds/src/App.jsx
+++ meds/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import ReactGA from 'react-ga'
 import Home from './pages/Home'
 import Tour from './pages/Tour'
 import Bio from './pages/Bio'
@@ -12,9 +13,32 @@ import Footer from './components/Footer'
 import './App.css'
 
 function App() {
+  // initializing GA for metrics bb
+  ReactGA.initialize('G-MWZRCQCMTS')
+
+  // ping the site user's location using GA
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search)
+  }, [location]);
+
+
+  function handleClick() {
+    // track a custom event with GA
+    ReactGA.event({
+      category: 'Button Click',
+      action: 'Click on any Link',
+      label: 'Link Tracking',
+    })
+    { onclick }
+  }
+
+  handleClick()
+
   const getYear = () => {
     return new Date().getUTCFullYear()
   }
+
 
   return (
     <div>
@@ -29,7 +53,7 @@ function App() {
           <Route path="/merch" element={<Merch />} />
           <Route path="/news" element={<News />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<PageNotFound/>} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </main>
     </div>
