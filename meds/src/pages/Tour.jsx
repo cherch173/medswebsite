ad260adfa35e776bb1bc804c3444e719f1a342a0
Cherch <b.cherchiglia@gmail.com>
2023-12-08 16:57:41 -0500

    glitch animation opacity alteration / nav spacing wider / code cleanup

--- meds/src/pages/Tour.jsx
+++ meds/src/pages/Tour.jsx
@@ -57,34 +57,34 @@ const Tour = () => {
                         </tr>
                         <br />
                         <tr>
-                                <td className="td">
-                                    TUE JAN 30, 2024
-                                </td>
-                                <td>
-                                    <p className="venueTD">
-                                        <Link target="_blank" className="supportA" to="https://www.instagram.com/sleepwalk.nyc">
-                                            Sleepwalk
-                                        </Link>
-                                    </p>
-                                </td>
-                                <td className="td">
-                                    Brooklyn, NY
-                                </td>
-                                <td className="td">
-                                    <i>
-                                        <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/3kQ55Rw5xE19eiAV3hLROi?si=KhsxCIZmSTGMp4nfZw6kZg">
-                                            Nite Music
-                                        </Link>
-                                        <br />
-                                        & TBA
-                                    </i>
-                                </td>
-                                <td className="td">
-                                    <Link target="_blank" to="https://link.dice.fm/M75085ad7d07">
-                                        <button className="onSaleButton">on sale</button>
+                            <td className="td">
+                                TUE JAN 30, 2024
+                            </td>
+                            <td>
+                                <p className="venueTD">
+                                    <Link target="_blank" className="supportA" to="https://www.instagram.com/sleepwalk.nyc">
+                                        Sleepwalk
                                     </Link>
-                                </td>
-                            </tr>
+                                </p>
+                            </td>
+                            <td className="td">
+                                Brooklyn, NY
+                            </td>
+                            <td className="td">
+                                <i>
+                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/3kQ55Rw5xE19eiAV3hLROi?si=KhsxCIZmSTGMp4nfZw6kZg">
+                                        Nite Music
+                                    </Link>
+                                    <br />
+                                    & TBA
+                                </i>
+                            </td>
+                            <td className="td">
+                                <Link target="_blank" to="https://link.dice.fm/M75085ad7d07">
+                                    <button className="onSaleButton">on sale</button>
+                                </Link>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
