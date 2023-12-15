8e83718b352144923c52e7c47baf695f87ebfed3
Cherch <b.cherchiglia@gmail.com>
2023-12-15 07:34:16 -0500

    tour dates updated

--- meds/src/pages/Tour.jsx
+++ meds/src/pages/Tour.jsx
@@ -34,10 +34,9 @@ const Tour = () => {
                             </td>
                             <td className="td">
                                 <i>
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/20u1eNEFrgn9XCwUGuBNt9?si=HysKacO6QIeVijRbyMB4Qg">
-                                        Brunch
+                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/03n10SXHwslbZktFzW0OGT?si=KCqn4N6tRxWF4IAdcA6doA">
+                                        Bayaz
                                     </Link>
-                                    &nbsp;(VT)
                                     <br />
                                     <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/2AUvhMfDlJMpAyPIHy9i8a?si=cc-L5MYNSouzb22zZg5D6w">
                                         Cold Sugar
