ceeb66c9f0b8b0d66cdd97fd9ce0a7533f1421ab
Cherch <b.cherchiglia@gmail.com>
2024-01-23 13:11:25 -0500

    tour page edit

--- meds/src/pages/Tour.jsx
+++ meds/src/pages/Tour.jsx
@@ -20,57 +20,6 @@ const Tour = () => {
                         <br />
                         <tr>
                             <td className="td">
-                                FRI JAN 19, 2024
-                            </td>
-                            <td>
-                                <p className="venueTD">
-                                    <Link target="_blank" className="supportA" to="https://www.instagram.com/tgn_bk">
-                                        <strong>The Grassy Noll</strong>
-                                        <br />
-                                        Cherch Presents:
-                                        <br />
-                                        <i>Harmonic Ontology
-                                            <br />
-                                            Episode 1</i>
-                                    </Link>
-                                </p>
-                            </td>
-                            <td className="td">
-                                Brooklyn, NY
-                            </td>
-                            <td className="td">
-                                <i>
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/1ZS6JkgfS2sUjAnP9SrK0d?si=rM0HiFdRQFiXL1MNBZh-Zg">
-                                        Meds (solo)
-                                    </Link>
-                                    <br />
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/600GgDUq9kzGJgNIrVsWfz?si=Auhos1FcQJy75wU1AbV_Pg">
-                                        Ana Becker (Catty, Habibi)
-                                    </Link>
-                                    <br />
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/7ILisSfJlzl4DBOoPvnHz6?si=5mn8mmCLR9euLlvTTUVVoA">
-                                        Sean Spada
-                                    </Link>
-                                    <br />
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/album/2QHYfdHq982XzvZta0WSGO?si=isUymQ3mTIW-JoGCCRy9XA">
-                                        Rose Blanshei
-                                    </Link>
-                                    <br />
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/1lAzczFXXpEWQoDFkgScRL?si=3El-oMDKTpWgn5-f3c9liw">
-                                        Brian LaRue (Safe Houses)
-                                    </Link>
-                                </i>
-                                <br />
-                            </td>
-                            <td className="td">
-                                <Link target="_blank" to="https://www.eventbrite.com/e/harmonic-ontology-episode-i-tickets-779859048407">
-                                    <button className="onSaleButton">on sale</button>
-                                </Link>
-                            </td>
-                        </tr>
-                        <br />
-                        <tr>
-                            <td className="td">
                                 TUE JAN 30, 2024
                             </td>
                             <td>
