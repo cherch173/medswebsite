cb5fc64cb0f9596c4c8dcc99e9ae163a524f1290
Cherch <b.cherchiglia@gmail.com>
2024-02-02 09:26:21 -0500

    tour page clean

--- meds/src/pages/Tour.jsx
+++ meds/src/pages/Tour.jsx
@@ -24,37 +24,6 @@ const Tour = () => {
                             </td>
                             <td>
                                 <p className="venueTD">
-                                    <Link target="_blank" className="supportA" to="https://www.instagram.com/sleepwalk.nyc">
-                                        <strong>Sleepwalk</strong>
-                                        <br />
-                                        curated by:
-                                        <br />
-                                        <i>Cameo Presents</i>
-                                    </Link>
-                                </p>
-                            </td>
-                            <td className="td">
-                                Brooklyn, NY
-                            </td>
-                            <td className="td">
-                                <i>
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/3kQ55Rw5xE19eiAV3hLROi?si=KhsxCIZmSTGMp4nfZw6kZg">
-                                        Nite Music
-                                    </Link>
-                                </i>
-                            </td>
-                            <td className="td">
-                                <Link target="_blank" to="https://link.dice.fm/M75085ad7d07">
-                                    <button className="onSaleButton">on sale</button>
-                                </Link>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="td">
-                                TUE JAN 30, 2024
-                            </td>
-                            <td>
-                                <p className="venueTD">
                                     <Link target="_blank" className="supportA" to="https://www.instagram.com/tgn_bk">
                                         <strong>The Grassy Noll</strong>
                                         <br />
