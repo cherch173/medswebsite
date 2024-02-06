ce67d6976ed3d0752b6897aa2764ff81aa4e3110
Cherch <b.cherchiglia@gmail.com>
2024-02-06 09:35:27 -0500

    url swap

--- meds/src/pages/Tour.jsx
+++ meds/src/pages/Tour.jsx
@@ -94,7 +94,7 @@ const Tour = () => {
                                 </i>
                             </td>
                             <td className="td">
-                                <Link target="_blank" to="https://www.alphavillebk.com">
+                                <Link target="_blank" to="https://dice.fm/event/ax9gx-meds-ian-st-george-bayaz-22nd-feb-alphaville-new-york-tickets">
                                     <button className="onSaleButton">on sale</button>
                                 </Link>
                             </td>
