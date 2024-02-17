9567eee3c7bd2b14c2ca4aba262207ee88029869
Cherch <b.cherchiglia@gmail.com>
2024-02-17 10:47:37 -0500

    tour page updated

--- meds/src/pages/Tour.jsx
+++ meds/src/pages/Tour.jsx
@@ -17,55 +17,6 @@ const Tour = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <br />
-                        <tr>
-                            <td className="td">
-                                TUE JAN 30, 2024
-                            </td>
-                            <td>
-                                <p className="venueTD">
-                                    <Link target="_blank" className="supportA" to="https://www.instagram.com/tgn_bk">
-                                        <strong>The Grassy Noll</strong>
-                                        <br />
-                                        <i>Meds Presents:</i>
-                                        <br />
-                                        <strong>Harmonic Ontology Episode II</strong>
-                                    </Link>
-                                </p>
-                            </td>
-                            <td className="td">
-                                Brooklyn, NY
-                            </td>
-                            <td className="td">
-                                <i>
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/3kQ55Rw5xE19eiAV3hLROi?si=KhsxCIZmSTGMp4nfZw6kZg">
-                                        Jamie Frey
-                                    </Link>
-                                    <br />
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/4aCJXSWJ1OXSyULyYhCua2?si=4PiaATtMTdiuqCWnJU90-A">
-                                        Mike Borchardt
-                                    </Link>
-                                    <br />
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/70C7Nli7wLiQXza0YUPe7E?si=O1I6Edz_T86j1pbeQ1MNIA">
-                                        Chris Urriola
-                                    </Link>
-                                    <br />
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/4Puycf8k2igE6Qth1dMWeS?si=VhOInJOsRo-GoxTMMZKw0Q">
-                                        Melody Henry
-                                    </Link>
-                                    <br />
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/15wXdPQif1yMmZfsF5y0kR?si=QZpB2sw_QcWKKMk1xghOfg">
-                                        Mike Cicchetti
-                                    </Link>
-                                    <br />
-                                </i>
-                            </td>
-                            <td className="td">
-                                <Link target="_blank" to="https://www.eventbrite.com/e/harmonic-ontology-episode-2-tickets-810881236607?aff=oddtdtcreator">
-                                    <button className="onSaleButton">on sale</button>
-                                </Link>
-                            </td>
-                        </tr>
                         <tr>
                             <td className="td">
                                 THU FEB 22, 2024
@@ -95,6 +46,43 @@ const Tour = () => {
                             </td>
                             <td className="td">
                                 <Link target="_blank" to="https://dice.fm/event/ax9gx-meds-ian-st-george-bayaz-22nd-feb-alphaville-new-york-tickets">
+                                    <button className="onSaleButton">on sale</button>
+                                </Link>
+                            </td>
+                        </tr>
+                        <br />
+                        <tr>
+                            <td className="td">
+                                WEDS MAR 20, 2024
+                            </td>
+                            <td>
+                                <p className="venueTD">
+                                    <Link target="_blank" className="supportA" to="https://www.instagram.com/tgn_bk">
+                                        <strong>The Grassy Noll</strong>
+                                        <br />
+                                        <i>Meds Presents:</i>
+                                        <br />
+                                        <strong>Harmonic Ontology Episode III</strong>
+                                    </Link>
+                                </p>
+                            </td>
+                            <td className="td">
+                                Brooklyn, NY
+                            </td>
+                            <td className="td">
+                                <i>
+                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/3kQ55Rw5xE19eiAV3hLROi?si=KhsxCIZmSTGMp4nfZw6kZg">
+                                        Cherch (Meds)
+                                    </Link>
+                                    <br />
+                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/4aCJXSWJ1OXSyULyYhCua2?si=4PiaATtMTdiuqCWnJU90-A">
+                                        & TBA
+                                    </Link>
+                                    <br />
+                                </i>
+                            </td>
+                            <td className="td">
+                                <Link target="_blank" to="https://www.eventbrite.com/e/harmonic-ontology-episode-2-tickets-810881236607?aff=oddtdtcreator">
                                     <button className="onSaleButton">on sale</button>
                                 </Link>
                             </td>
