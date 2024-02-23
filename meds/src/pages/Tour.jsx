9fa11f1e13155a0639e5159615639f8978cac2b8
Cherch <b.cherchiglia@gmail.com>
2024-02-23 11:57:03 -0500

    tour date edits

--- meds/src/pages/Tour.jsx
+++ meds/src/pages/Tour.jsx
@@ -19,36 +19,6 @@ const Tour = () => {
                     <tbody>
                         <tr>
                             <td className="td">
-                                THU FEB 22, 2024
-                            </td>
-                            <td>
-                                <p className="venueTD">
-                                    <Link target="_blank" className="supportA" to="https://www.instagram.com/alphavillebk">
-                                        <strong>Alphaville</strong>
-                                    </Link>
-                                </p>
-                            </td>
-                            <td className="td">
-                                Brooklyn, NY
-                            </td>
-                            <td className="td">
-                                <i>
-
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/151m7o9ULqLUt8t54dE79Y?si=XseUfAg6QVKxGYcODJuNhQ">
-                                        Ian St. George
-                                    </Link>
-                                    <br />
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/03n10SXHwslbZktFzW0OGT?si=Ab073uT7S2SgXxVom3Mimg">
-                                        Bayaz
-                                    </Link>
-                                    <br />
-                                    <Link className="supportA" target="_blank" to="https://open.spotify.com/artist/2d11uzrnCsKBwyDzyotbdA?si=_0yOVn_aQomAQQ3Z0FJ5BA">
-                                        Jet Daisy
-                                    </Link>
-                                    <br />
-                                </i>
-                            </td>
-                            <td className="td">
                                 <Link target="_blank" to="https://dice.fm/event/ax9gx-meds-ian-st-george-bayaz-22nd-feb-alphaville-new-york-tickets">
                                     <button className="onSaleButton">on sale</button>
                                 </Link>
