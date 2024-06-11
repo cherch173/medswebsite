3d583a5e9261b79d70247d08929d18e246370d05
Cherch <b.cherchiglia@gmail.com>
2024-06-11 10:04:53 -0400

    festival added

--- meds/src/pages/Tour.jsx
+++ meds/src/pages/Tour.jsx
@@ -23,35 +23,35 @@ const Tour = () => {
                         <br />
                         <tr>
                             <td className="td">
-                                <Link target="_blank" className="supportA" to="https://link.dice.fm/B5LpnmimkJb?sharer_id=64a6e811a667430001f571a7">
-                                    THU 6.6.2024
+                                <Link target="_blank" className="supportA" to="https://www.sugarloafpacny.com/shows">
+                                    SAT 8.31.2024
                                 </Link>
                             </td>
                             <td>
                                 <p className="venueTD">
-                                    <Link target="_blank" className="supportA" to="https://www.instagram.com/ourwickedlady">
-                                        <strong>Our Wicked Lady</strong>
+                                    <Link target="_blank" className="supportA" to="https://www.sugarloafpacny.com">
+                                        <strong>DEAD AHEAD FESTIVAL</strong>
 										<br />
-										(doors 7pm)
+										@ Sugar Loaf Performing Arts Center
                                     </Link>
                                 </p>
                             </td>
                             <td className="td">
-                                <Link target="_blank" className="supportA" to="hhttps://link.dice.fm/B5LpnmimkJb?sharer_id=64a6e811a667430001f571a7">
-                                    Brooklyn, NY
+                                <Link target="_blank" className="supportA" to="https://www.sugarloafpacny.com/shows">
+                                    Chester, NY
                                 </Link>
                             </td>
                             <td className="td">
-                                <Link target="_blank" className="supportA" to="https://www.instagram.com/cooldeadwoman">
-                                    <i>Cool Dead Woman</i>
+                                <Link target="_blank" className="supportA" to="https://www.instagram.com/zachdeputy">
+                                    <i>Zach Deputy (two sets)</i>
                                 </Link>
                                 {" "}&{" "}
-                                <Link target="_blank" className="supportA" to="https://www.instagram.com/chick_eddie">
-                                    <i>Chick Eddie</i>
+                                <Link target="_blank" className="supportA" to="https://www.instagram.com/steelydeadband">
+                                    <i>Steely Dead (two sets)</i>
                                 </Link>
                             </td>
                             <td className="td">
-                                <Link target="_blank" to="https://link.dice.fm/B5LpnmimkJb?sharer_id=64a6e811a667430001f571a7">
+                                <Link target="_blank" to="https://www.sugarloafpacny.com/shows">
                                     <button className="onSaleButton">on sale</button>
                                 </Link>
                             </td>
